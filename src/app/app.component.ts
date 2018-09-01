2821ca1496cd260b058d96ad8aa5402a3307eb83
Yeison Niño <ingydnino@hotmail.com>
2018-08-31 19:17:26 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //define quien es el selector 
+  templateUrl: './app.component.html', //
+  styleUrls: ['./app.component.css'] //define quien tiene los estilos
 })
 export class AppComponent {
-  title = 'ClasedeAngular';
+  title = 'JValdez';
 }
